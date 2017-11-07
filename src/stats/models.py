5cd4e2d61c4d3e28fd6544ad5963270943354fbf
Chif Gergo <chifgeri97@gmail.com>
2017-11-07 20:20:28 +0100

    Modellek aprobb valtoztatasai

--- src/stats/models.py
+++ src/stats/models.py
@@ -5,8 +5,16 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     year_of_join = models.IntegerField()
-    user = models.OneToOneField(User)
-    # Homeworks=models.ForeignKey(Homework);
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    TEAMS = (
+    ( 'DT', 'DevTeam'),
+    ('NET', 'NeTeam'),
+    ('ST','SecurITeam'),
+    ('SYS','SysAdmin'),
+    ('N','None'),
+    )
+    pref_group = models.CharField(max_length=10, choices = TEAMS, default = 'None')
+    # Homeworks=models.ForeignKey(Homework)
 
     def __str__(self):
         return self.user.username
@@ -14,5 +22,6 @@ class Profile(models.Model):
 
 class KszkEvent(models.Model):
     date = models.DateField()
+    visitors = models.ManyToManyField(User, related_name = 'visitor')
+
     num_of_pers = models.IntegerField()
-    visitors = models.ManyToManyField(User)
