eed3cbc440e337a243f3781ab8b6c71f7998aeea
Chif Gergo <chifgeri97@gmail.com>
2017-12-23 16:11:37 +0100

    Profile create serializer modositasok

--- src/account/serializers.py
+++ src/account/serializers.py
@@ -1,21 +1,49 @@
 from rest_framework import serializers
-from . import models
+from account.models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Profile
+        model = Profile
         fields = '__all__'
 
 
 class ProfileCreateSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+    # Create a custom method field
+    current_user = serializers.SerializerMethodField('_user')
+
     class Meta:
-        model = models.Profile
+        model = Profile
         fields = [
-                'user.username',
+                'current_user',
+                'first_name',
+                'last_name',
+                'email',
                 'nick',
                 'pref_group',
                 'signed',
                   ]
 
-    # def create(self, *args, **kwargs, obj):
+    def _user(self, obj):
+        user = self.context['request'].user
+        return user
+
+    def create(self, validated_data):
+        #user.first_name = validated_data['first_name']
+        #user.last_name = validated_data['last_name']
+        #user.email = validated_data['email']
+        #user.save()
+        nick = validated_data['nick']
+        pref_group = validated_data['pref_group']
+        signed = validated_data['signed']
+        profile_obj = Profile(
+                user=user,
+                nick=nick,
+                pref_group=pref_group,
+                signed=signed,
+        )
+        profile_obj.save()
+        return validated_data
