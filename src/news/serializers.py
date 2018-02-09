329a275e5fe6a9f83058159cde4298e659573c8b
Barnabás Czémán <trabarni@gmail.com>
2018-02-09 17:46:13 +0100

    Add author full name to news response.

--- src/news/serializers.py
+++ src/news/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
+    author_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Article
         fields = serializers.ALL_FIELDS
+
+    def get_author_name(self, obj):
+        return obj.author.user.get_full_name()
