69e1282ddb1845e559ce46413aebaa6b1b38fc38
Barnabás Czémán <trabarni@gmail.com>
2018-02-14 18:02:32 +0100

    Add signed and groups for resource

--- src/account/resources.py
+++ src/account/resources.py
@@ -1,15 +1,22 @@
-from import_export import resources
+from import_export import resources, widgets, fields
 from . import models
 
 
 class SignUpResource(resources.ModelResource):
+    groups = fields.Field(
+        attribute='groups',
+        widget=widgets.ManyToManyWidget(model=models.GroupChoice, separator=' ,', field='choice'),
+    )
+
     class Meta:
         model = models.Profile
         fields = (
             'user__first_name',
             'user__last_name',
             'user__email',
+            'groups',
             'motivation_about',
             'motivation_profession',
             'motivation_exercise',
+            'signed',
         )
