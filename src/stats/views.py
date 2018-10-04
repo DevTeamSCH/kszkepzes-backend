9f4d1e4d7d4db1b701a5ec40b175192f2b8deaa5
Bodor Máté <bmate711@gmail.com>
2018-10-04 17:18:23 +0200

    Add parmissions to event views

--- src/stats/views.py
+++ src/stats/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets
 
 from . import models
 from . import serializers
-
+from common.permissions import IsStaffUser
 
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.EventSerializer
     queryset = models.Event.objects.all()
+    permission_classes = [IsStaffUser]
