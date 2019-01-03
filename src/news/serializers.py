48ea4b846525f57e67f114b0994ae7b69b9512f5
Bodor Máté <bodor.mate@kszk.bme.hu>
2019-01-03 21:41:00 +0100

    refactor serializer

--- src/news/serializers.py
+++ src/news/serializers.py
@@ -5,12 +5,12 @@ from common.serializers import CurrentUserProfileDefault
 
 class ArticleSerializer(serializers.ModelSerializer):
     updated_by = serializers.HiddenField(default=CurrentUserProfileDefault())
-    last_updated_by = serializers.SerializerMethodField()
+    last_update_by = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
         read_only_fields = ('author', 'created_at', 'updated_at', 'updated_by')
         fields = '__all__'
 
-    def get_last_updated_by(self, obj):
+    def get_last_update_by(self, obj):
         return obj.updated_by.id
