1dbb64a38eccb3483aaeca8d99f7114116f26101
Bodor Máté <bodor.mate@kszk.bme.hu>
2019-01-04 20:41:21 +0100

    add visitor_number field to the serializer

--- src/stats/serializers.py
+++ src/stats/serializers.py
@@ -6,14 +6,18 @@ from . import models
 class EventSerializer(serializers.ModelSerializer):
     created_by = serializers.HiddenField(default=CurrentUserProfileDefault())
     created_by_name = serializers.SerializerMethodField()
+    visitor_number = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Event
         fields = '__all__'
         read_only_fields = ('created_at', 'update_at', 'created_by')
 
-    def get_creted_by_name(self, obj):
+    def get_created_by_name(self, obj):
         return obj.created_by.full_name
+
+    def get_visitor_number(self, obj):
+        return obj.visitors.all().count()
 
 
 class NoteSerializer(serializers.ModelSerializer):
