1c602ae89b93e6116ac6464a5db13113702a2593
Bodor Máté <bodor.mate@kszk.bme.hu>
2019-01-14 17:01:00 +0100

    Add created_by field to serializer

--- src/homework/models.py
+++ src/homework/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.core import validators
-
-from common.validators import FileSizeValidator
+from common.middleware import CurrentUserMiddleware
 from account.models import Profile
 
 
-
 class Task(models.Model):
-    created_by = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    created_by = models.ForeignKey(
+        Profile,
+        on_delete=models.DO_NOTHING,
+        related_name='tasks',
+        default=CurrentUserMiddleware.get_current_user_profile,
+    )
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     title = models.CharField(max_length=150)
@@ -19,8 +21,13 @@ class Task(models.Model):
 
 
 class Solution(models.Model):
-    task = models.ForeignKey(Task, related_name='solution', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(Profile, related_name='solution', on_delete=models.DO_NOTHING)
+    task = models.ForeignKey(Task, related_name='solutions', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        Profile,
+        related_name='solution',
+        on_delete=models.DO_NOTHING,
+        default=CurrentUserMiddleware.get_current_user_profile,
+    )
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     note = models.TextField()
--- src/homework/serializers.py
+++ src/homework/serializers.py
@@ -6,8 +6,6 @@ from . import models
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    created_by = serializers.HiddenField(default=CurrentUserProfileDefault())
-
     class Meta:
         model = models.Task
         read_only_fields = ('created_by', 'created_at', 'updated_at')
@@ -20,10 +18,6 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
 class SolutionSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
-    created_by = serializers.HiddenField(default=CurrentUserProfileDefault())
-
     class Meta:
         model = models.Solution
         read_only_fields = ('created_by', 'created_at', 'updated_at', 'ready', 'files')
