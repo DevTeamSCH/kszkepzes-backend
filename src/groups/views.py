4e4df03669ac3334f76340f61f72930bf553e8c0
rlacko <rlacko@sch.bme.hu>
2020-01-26 17:32:10 +0100

    fix

--- src/groups/views.py
+++ src/groups/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from . import serializers
 from . import models
 
-class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+class GroupsViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.GroupSerializer
     queryset = models.Group.objects.all()
     
